7df238edc42c03eb607a0975e02272ab5180801e
Elliot Iddon <elliot@citrusbytes.net>
2021-02-19 17:24:15 +0000

    Inital commit, demo app which uses swapi
    express app with one endpoint `/endpoint` which fetches `id` of `type` from
    swapi and optionally includes the films the entity is in.
    
    Control of config is passed to Ambeeance which can then be used to modify the
    values on the fly without app restart as well as persisting values across app
    runs.

---
+++ index.js
@@ -0,0 +1,28 @@
+const { ambeeance } = require('../ambeeance-client')
+const axios = require('axios');
+const express = require('express');
+require('express-async-errors');
+const app = express();
+
+const config = ambeeance({
+  type: 'planets',
+  id: 1,
+  include_films: false,
+});
+
+const swapiUrl = 'https://swapi.dev/api';
+
+app.get('/endpoint', async (req, res) => {
+  const swapiResponse = await axios.get(`${swapiUrl}/${config.type}/${config.id}`);
+  const result = {
+    name: swapiResponse.data.name
+  };
+
+  if (config.include_films) {
+    result.films = swapiResponse.data.films
+  }
+  
+  res.status(200).send(result);
+});
+
+app.listen(4001);
