27bbcb0251eab5d881d01f45aaf44991eeb3626b
Elliot Iddon <elliot@citrusbytes.net>
2021-04-23 13:59:27 +0100

    Use ambeeance loader

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ambeeance } = require('ambeeance-client')
+const { ambeeance } = require('ambeeance-loader')(require)
 const axios = require('axios');
 const express = require('express');
 require('express-async-errors');
